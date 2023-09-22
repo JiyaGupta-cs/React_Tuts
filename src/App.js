1a21967db76525f68849c3c2bb11e983bcd1619b
Jiya <coderaholic21@gmail.com>
2023-09-22 15:21:54 +0530

    Enabling Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+
 
 let name = "Jiya";
 
 function App() {
+  const [mode, setMode] = useState('light'); //Whether DarkMode is enabled or not
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743';
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="Text Utils" abouttext="AboutText" />
+      <Navbar title="TextTrove" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-      <TextForm heading="Enter the text to analyze" />
+        <TextForm mode={mode} heading="Enter the text to analyze" />
+        <About  mode={mode}/>
       </div>
-      
+
     </>
   );
 }
