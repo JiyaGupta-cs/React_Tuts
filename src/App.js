7c0aa2dd7464faa7f4cda0efa42432fa2d1c10f6
Jiya <coderaholic21@gmail.com>
2023-09-24 13:18:00 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-
-let name = "Jiya";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //Whether DarkMode is enabled or not
@@ -15,31 +14,44 @@ function App() {
       msg: message,
       typ: type
     })
-    setTimeout(() =>{
+    setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
   }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled","success");
+      showAlert("Dark mode has been enabled", "success");
     }
     else {
       setMode('light')
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled","success");
+      showAlert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
-      <Navbar title="TextTrove" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze" />
-        <About mode={mode} />
-      </div>
+      <Router>
+        <Navbar title="TextTrove" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          {/* <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze" />
+            </Route>
+          </Switch> */}
+          <Routes>
+    <Route exact path="/" element={<TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze" />} />
+    <Route exact path="/about" element={<About mode={mode} />} />
+</Routes>
 
+          {/* <About mode={mode} /> */}
+        </div>
+      </Router>
     </>
   );
 }
