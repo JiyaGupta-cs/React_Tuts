446b4eca24b5b100690bed5088f52fb674f24377
Jiya <coderaholic21@gmail.com>
2023-09-24 13:41:24 +0530

    Publishing on Github Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //Whether DarkMode is enabled or not
@@ -32,26 +32,19 @@ function App() {
   }
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextTrove" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          {/* <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
-              <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze" />
-            </Route>
-          </Switch> */}
-          <Routes>
+          {/* <Routes>
     <Route exact path="/" element={<TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze" />} />
     <Route exact path="/about" element={<About mode={mode} />} />
-</Routes>
+</Routes> */}
 
           {/* <About mode={mode} /> */}
+          <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze" />
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
